13e5a980bafb1c73fe3bba1230d9888feeee3aa8
DarshilModi29 <132668473+DarshilModi29@users.noreply.github.com>
2024-10-09 16:36:58 +0530

    Change about page and change pacakge-lock and package.json because of eslint error

--- frontend/src/views/About.js
+++ frontend/src/views/About.js
@@ -7,27 +7,19 @@ export default function About() {
         <div className="row justify-content-md-center">
           <div className="col-md-10 col-sm-12">
             <div className="row">
+              <div className="col-md-8 mb-3 col-sm-12">
+                <h2>Donec rutrum congue leo eget malesuada lacinia eget consectetur.</h2>
+              </div>
               <div className="col-xl-12 col-md-10 col-sm-12">
                 <p>
                   Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla quis lorem ut libero malesuada feugiat. Pellentesque in ipsum id orci porta dapibus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.
                   <br /><br />
                   Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Curabitur aliquet quam id dui posuere blandit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus.
+                  <br /><br />
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, tenetur veritatis. Incidunt, eius! Inventore ut iusto odit libero! Asperiores, dicta!
                 </p>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
-      <div className="container additional">
-        <div className="row">
-          <div className="col-xl-4">
-            <img src="img/photo-12.jpg" className="img-fluid" alt="" />
-          </div>
-          <div className="col-xl-4">
-            <img src="img/photo-13.jpg" className="img-fluid" alt="" />
-          </div>
-          <div className="col-xl-4">
-            <img src="img/photo-14.jpg" className="img-fluid" alt="" />
           </div>
         </div>
       </div>
