c129cc2d1c62dc820db678b3fbfdd0ea25447c1a
DarshilModi29 <132668473+DarshilModi29@users.noreply.github.com>
2024-12-21 14:18:37 +0530

    Remove bug

--- frontend/src/views/Images.js
+++ frontend/src/views/Images.js
@@ -19,6 +19,7 @@ export default function Images() {
       navigate(`/images/${selectedTags.value}`);
       setImages([]);
     } else {
+      setImages([]);
       navigate("/images/all");
     }
   }
